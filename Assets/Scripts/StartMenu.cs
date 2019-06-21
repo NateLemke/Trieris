b4d35fbbc49a067817e8945601dd1d2f49dd741b
AKing58 <35930720+AKing58@users.noreply.github.com>
2019-06-21 02:52:18 -0700

    testing mp start game

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 /// <summary>
 /// Contains functions that are used for the buttons on the main menu.
 /// </summary>
-public class StartMenu : MonoBehaviourPun
+public class StartMenu : MonoBehaviour
 {
     void Start()
     {
