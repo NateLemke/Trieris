ddc65cd7f66107e5d4c1751248cce238056ab4c2
Adam <adamkdevelop@gmail.com>
2019-08-26 13:31:37 -0700

    fixed wrong gameobject reference

--- Assets/Scripts/UIControl.cs
+++ Assets/Scripts/UIControl.cs
@@ -798,7 +798,7 @@ public class UIControl : MonoBehaviour
         controlsEnabled = false;
         goButton.enabled = false;
         if(PhotonNetwork.IsConnected){
-            GameObject.Find("OverlayCanvas/BottomUIPanel/ReadyUpLegend").gameObject.SetActive(false);
+            GameObject.Find("OverlayCanvas/UIBottomPanel/ReadyUpLegend").gameObject.SetActive(false);
         }
         foreach(Button b in commandPanels)
         {
